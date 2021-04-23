06f52590cac8ee32f207c1bbf820d3cfd33a11ab
judge2010 <74088083+judge2010@users.noreply.github.com>
2021-04-23 16:34:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-function preload(){
-
-};
-var left_wrist;
-var right_wrist;
+song = "";
+var left_wrist_x;
+var right_wrist_x;
 var right_wrist_y;
 var left_wrist_y;
+function preload(){
+
+    song=loadSound("music.mp3");
+
+};
 
 function draw(){
 image(video ,0 ,0 ,500 ,450 );
@@ -23,9 +26,13 @@ console.log("Model is loaded");
 function gotPoses(results){
     if(results.length > 0){
 console.log(results);
-left_wrist=results[0].pose.leftWrist.x;
-right_wrist=results[0].pose.rightWrist.x;
+left_wrist_x=results[0].pose.leftWrist.x;
+right_wrist_x=results[0].pose.rightWrist.x;
 left_wrist_y=results[0].pose.leftWrist.y;
 right_wrist_y=results[0].pose.rightWrist.y;
     };
+};
+function Play_m(){
+
+    song.play();
 };
